eb334a13d52439f70cb280389ccb7072f43b026b
Lautaro Brito <lautarobrito@hotmail.com>
2020-08-30 21:41:08 -0300

    último arreglo adivinanza

--- adivinanza.js
+++ adivinanza.js
@@ -2,7 +2,7 @@ var c = 4
 
 function contar() {
     c -= 1
-    document.getElementById("intentos").innerHTML = ("Tienes: "+c + " intentos")
+    document.getElementById("intentos").innerHTML = (c + " intentos")
     if (c == 0) {
         document.getElementById("resultado").innerHTML = ("Te quedaste sin intentos")
     }
@@ -12,21 +12,21 @@ function contar() {
 function capturar() {
     document.getElementById("btns2").disabled = true
     var respuesta = document.getElementById("Resp").value;
-    if (respuesta === "blanco" ) {
-        document.getElementById("resultado").innerHTML = ("ganaste!! sos un genio!! era blanco!!");
+    if (respuesta === "Norberto Silva" ) {
+        document.getElementById("resultado").innerHTML = ("ganaste!! sos un genio!! era  Norberto Silva!!");
         document.getElementById("btns").disabled = true
         document.getElementById("btns2").disabled = false
     }
     else {
         document.getElementById("resultado").innerHTML = ("no es correcto te quedan: ");
     }
-    if (c == 2 && respuesta != "blanco") {
-        document.getElementById("pista1").innerHTML = ("Pista 1: empieza con b")
+    if (c == 2 && respuesta != "Norberto Silva") {
+        document.getElementById("pista1").innerHTML = ("Pista 1: su nombre empieza con N")
     }
-    if (c == 1 && respuesta != "blanco") {
-        document.getElementById("pista2").innerHTML = ("Pista 2: termina con o")
+    if (c == 1 && respuesta != "Norberto Silva") {
+        document.getElementById("pista2").innerHTML = ("Pista 2: su apellido empieza con S")
     }
-    if (c === 0 && respuesta != "blanco" )  {
+    if (c === 0 && respuesta != "Norberto Silva" )  {
         document.getElementById("resultado").innerHTML = ("Te quedaste sin intentos")
         document.getElementById("btns").disabled = true
         document.getElementById("btns2").disabled = false
